ccf7eafcc79127f31cf61ca3ba87384c851b3530
Anna Sophia Monzel <46888826+annamonzel@users.noreply.github.com>
2025-02-04 17:30:28 -0500

    Add code via upload

---
+++ Code/Figure5/MitoPPS_calculation.R
@@ -0,0 +1,149 @@
+rm(list = ls())
+
+
+# Setup -------------------------------------------------------------------
+
+library(tidyverse)
+
+# Assing tissue groups
+tissue_to_group_hm <- function(x) {
+  mutate(x, Group = case_when(
+    (Tissue == "bone marrow" | Tissue =="thymus" | Tissue =="tonsil" | Tissue =="lymph node" | Tissue =="spleen")~"Immune",
+    (Tissue == "placenta" | Tissue == "ovary" | Tissue == "seminal vesicle" | Tissue == "cervix" | Tissue == "prostate" | 
+       Tissue == "endometrium" | Tissue == "fallopian tube" | Tissue == "vagina" | Tissue =="epididymis" | Tissue == "testis") ~ "Reproductive",
+    (Tissue == "gallbladder" | Tissue == "duodenum" | Tissue == "small intestine" | 
+       Tissue == "stomach" | Tissue == "rectum" | Tissue == "colon"| Tissue == "esophagus"| Tissue == "appendix") ~ "Digestive",
+    (Tissue == "cerebellum" | Tissue ==  "white matter" | Tissue ==  "cerebral cortex" | Tissue ==  "choroid plexus" | 
+       Tissue ==  "thalamus" | Tissue ==  "hypothalamus" | Tissue ==  "medulla oblongata" | Tissue ==  "basal ganglia" | 
+       Tissue ==  "pons" | Tissue ==  "spinal cord" | Tissue ==  "midbrain" | Tissue ==  "amygdala" | Tissue ==  "hippocampal formation" | 
+       Tissue == "olfactory bulb") ~"CNS",
+    (Tissue == "heart muscle" | Tissue == "skeletal muscle") ~ "Contractile",
+    (Tissue == "adrenal gland" | Tissue == "parathyroid gland" | Tissue == "pituitary gland"|
+       Tissue == "salivary gland" | Tissue == "thyroid gland" | Tissue == "adipose tissue" ) ~ "Secretory",
+    (Tissue == "liver" | Tissue == "kidney") ~ "Anabolic",
+    (Tissue == "lung" | Tissue == "pancreas"| Tissue == "skin"| Tissue == "smooth muscle" | 
+       Tissue == "tongue" | Tissue == "urinary bladder"| Tissue == "breast" | Tissue == "retina") ~ "Other"), 
+    .after = "Tissue")
+}
+
+## Get human mitocarta genes
+mitogenes <- readxl::read_xls(here::here("Data","MitoCarta","OriginalData","HumanMitoCarta3_0.xls"), sheet = 2) %>%
+  dplyr::rename(Gene = Symbol) %>%
+  select(Gene)
+mitogenes <- mitogenes$Gene
+# Load hpa data
+hpa_data <- read_tsv(here::here("Data","HumanProteinAtlas","OriginalData","rna_human_tissue_consensus.tsv"),show_col_types = FALSE)
+# Processed dataset 
+processed_data <-  hpa_data%>%
+  dplyr::rename(GeneName = `Gene name`) %>%
+  filter(GeneName %in% mitogenes) %>%
+  select(GeneName, Tissue, nTPM) %>%
+  na.omit() %>%
+  pivot_wider(names_from = "GeneName", values_from = "nTPM") %>%
+  select(-TSTD3) %>% # has too many NAs
+  mutate(Tissue = str_to_lower(Tissue)) %>%
+  tissue_to_group_hm() %>%
+  mutate(Tissue = str_to_sentence(Tissue))
+# Assing tissue groups
+
+# Create gene-to-pathway-list
+mitocarta_sheet4 <- readxl::read_xls(here::here("Data","MitoCarta", "OriginalData","HumanMitoCarta3_0.xls"), sheet = 4) %>%
+  dplyr::select(MitoPathway, Genes) %>%
+  na.omit() 
+gene_to_pathway <- splitstackshape::cSplit(mitocarta_sheet4, 'Genes', ',') %>%
+  column_to_rownames("MitoPathway") %>%
+  t() %>%
+  as.data.frame() 
+gene_to_pathway <- gene_to_pathway %>%
+  pivot_longer(cols = colnames(gene_to_pathway), names_to = "Pathway", values_to = "Gene") %>%
+  na.omit() 
+mitogenes <- readxl::read_xls(here::here("Data","MitoCarta", "OriginalData","HumanMitoCarta3_0.xls"), sheet = 2) %>%
+  select(Symbol)
+mitogenes <- mitogenes$Symbol
+
+levels <- readxl::read_xls(here::here("Data", "MitoCarta","OriginalData","HumanMitoCarta3_0.xls"), sheet = 4) %>%
+  select(MitoPathway, `MitoPathways Hierarchy`) %>%
+  separate(`MitoPathways Hierarchy`, into = c("Pathway_Level1", "Pathway_Level2", "Pathway_Level3"), sep = " > ") %>%
+  mutate(Level = case_when(
+    MitoPathway == Pathway_Level1 ~ "Pathway_Level1", 
+    MitoPathway == Pathway_Level2 ~ "Pathway_Level2", 
+    MitoPathway == Pathway_Level3 ~ "Pathway_Level3"
+  )) %>%
+  unique() %>%
+  dplyr::rename(Pathway = MitoPathway)
+
+
+data_pathways <- processed_data %>%
+  pivot_longer(cols = colnames(processed_data[3:ncol(processed_data)]), names_to = "Gene") %>%
+  full_join(gene_to_pathway, by = "Gene") %>%
+  filter(!is.na(Pathway)) %>%
+  unique()%>%
+  group_by(Tissue, Pathway) %>%
+  mutate(score = mean(value, na.rm = T)) %>%
+  select(-c(Gene, value)) %>%
+  unique() 
+
+write_csv(data_pathways,here::here("Data", "HumanProteinAtlas","ProcessedData", "Pathway_scores_all_tissues.csv" ))
+
+# Pathway ratios ----------------------------------------------------------
+
+all_pathways <- unique(data_pathways$Pathway)
+data_long <- data_pathways %>%
+  select(Tissue, Group, Pathway, score) 
+
+pw_pairs <- data.frame(pw1 = rep(all_pathways, each = 149)) %>%
+  group_by(pw1) %>%
+  nest() %>%
+  mutate(data = map(data, ~mutate(.x, pw2 = unique(all_pathways)))) %>%
+  unnest() %>%
+  filter(!pw1==pw2) 
+colnames(pw_pairs) <- c("pw1", "pw2")
+
+janitor::tabyl(data_long, Pathway)
+data_added1 <- pw_pairs %>%
+  ungroup() %>%
+  dplyr::select(pw1) %>%
+  dplyr::rename(Pathway = pw1) %>%
+  inner_join(data_long, by = "Pathway") %>%
+  unique() %>%
+  dplyr::rename(value1 = score, Pathway1 = Pathway) %>%
+  dplyr::slice(rep(row_number(), 148)) %>%
+  arrange(Pathway1) %>%
+  group_by(Tissue, Group) %>%
+  nest() %>%
+  dplyr::rename(pw1 = data) 
+
+data_added2 <- pw_pairs %>%
+  ungroup() %>%
+  dplyr::select(pw2) %>%
+  dplyr::rename(Pathway = pw2) %>%
+  inner_join(data_long, by = "Pathway") %>%
+  unique() %>%
+  dplyr::rename(value2 = score, Pathway2 = Pathway) %>%
+  dplyr::slice(rep(row_number(), 148)) %>%
+  arrange(desc(Pathway2)) %>%
+  unique() %>%
+  group_by(Tissue, Group) %>%
+  nest() %>%
+  dplyr::rename(pw2 = data) 
+
+data_ratios =full_join(data_added1, data_added2, by = c("Tissue", "Group")) %>%
+  mutate(combined =  map2(pw1, pw2, ~ cbind(.x,  .y))) %>%
+  select(-c(pw1, pw2)) %>%
+  unnest(cols = combined) %>%
+  unique() %>%
+  mutate(ratio = value1 / value2) %>%
+  group_by(Pathway1, Pathway2) %>%
+  mutate(average_ratio = mean(ratio)) %>%
+  mutate(corrected_ratio = ratio / average_ratio)
+
+mtPPS <- data_ratios %>%
+  filter(!is.na(corrected_ratio)) %>%
+  group_by(Tissue, Pathway1) %>%
+  mutate(mtPPS = mean(corrected_ratio)) %>%
+  select(Tissue, Group, Pathway1, mtPPS) %>%
+  unique() %>%
+  pivot_wider(names_from = "Pathway1", values_from = "mtPPS")
+
+write_csv(mtPPS,here::here("Data", "HumanProteinAtlas","ProcessedData", "mtPPS_all_tissues.csv" ))
+
